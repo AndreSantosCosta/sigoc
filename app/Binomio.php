73412dd24d4c54b0828fa070e5a22b107ebd3b6e
miriampereira <miriam.pereir@hotmail.com>
2015-11-13 18:01:25 +0000

    atualizarFormsGeral

--- app/Binomio.php
+++ app/Binomio.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Binomio extends Model
 {
-    //
+    public function getNumMecanogAttribute(){
+		$num = Militar::findOrFail($this->militar_id);
+		return $num->num_mecanografico;
+	}
+
+	public function getNumMatriculaAttribute(){
+		$numM = Cao::findOrFail($this->cao_id);
+		return $numM->num_matricula;
+	}
 }
--- resources/views/binomios/index.blade.php
+++ resources/views/binomios/index.blade.php
@@ -17,36 +17,30 @@
                 <table class="table table-condensed table-striped">
                     <thead>
                         <tr>
-                            <th>ID</th>
-                            <th>DATA_INICIO</th>
-                        <th>MILITAR_ID</th>
-                        <th>CAO_ID</th>
-                        <th>VERTENTE</th>
-                        <th>INATIVO</th>
-                        <th>DATA_INATIVO</th>
-                        <th>MOTIVO_INATIVO</th>
-                            <th class="text-right">OPTIONS</th>
+                        <th>Numero Mecanográfico</th>
+                        <th>Número Matricula</th>
+                        <th>Data de Inicio</th>
+                        <th>Vertente</th>
+                        <th>Inativo</th>
+                            <th class="text-right">Opções</th>
                         </tr>
                     </thead>
 
                     <tbody>
                         @foreach($binomios as $binomio)
                             <tr>
-                                <td>{{$binomio->id}}</td>
-                                <td>{{$binomio->data_inicio}}</td>
-                    <td>{{$binomio->militar_id}}</td>
-                    <td>{{$binomio->cao_id}}</td>
+                    <td>{{$binomio->numMecanog}}</td>
+                    <td>{{$binomio->numMatricula}}</td>
+                    <td>{{$binomio->data_inicio}}</td>
                     <td>{{$binomio->vertente}}</td>
-                    <td>{{$binomio->inativo}}</td>
-                    <td>{{$binomio->data_inativo}}</td>
-                    <td>{{$binomio->motivo_inativo}}</td>
+                    <td>{{$binomio->inativo}}</td> 
                                 <td class="text-right">
-                                    <a class="btn btn-xs btn-primary" href="{{ route('binomios.show', $binomio->id) }}"><i class="glyphicon glyphicon-eye-open"></i> View</a>
-                                    <a class="btn btn-xs btn-warning" href="{{ route('binomios.edit', $binomio->id) }}"><i class="glyphicon glyphicon-edit"></i> Edit</a>
+                                    <a class="btn btn-xs btn-primary" href="{{ route('binomios.show', $binomio->id) }}"><i class="glyphicon glyphicon-eye-open"></i> Ver</a>
+                                    <a class="btn btn-xs btn-warning" href="{{ route('binomios.edit', $binomio->id) }}"><i class="glyphicon glyphicon-edit"></i> Editar</a>
                                     <form action="{{ route('binomios.destroy', $binomio->id) }}" method="POST" style="display: inline;" onsubmit="if(confirm('Delete? Are you sure?')) { return true } else {return false };">
                                         <input type="hidden" name="_method" value="DELETE">
                                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                        <button type="submit" class="btn btn-xs btn-danger"><i class="glyphicon glyphicon-trash"></i> Delete</button>
+                                        <button type="submit" class="btn btn-xs btn-danger"><i class="glyphicon glyphicon-trash"></i> Apagar</button>
                                     </form>
                                 </td>
                             </tr>
