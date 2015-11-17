d447194a76050b34e36cf2d92d6daed5b65dc427
AndreSantosCosta <andredscosta@gmail.com>
2015-11-17 00:48:38 +0000

    Login

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -8,6 +8,19 @@
     <!-- Header -->
     <!--<div class="geral_container">-->
         <div class="container_login">
+        	{!! Form::open() !!}
+
+				<div class="form-group">
+					{!! Form::label('name', 'Username:') !!}
+					{!! Form::text('name') !!}
+				</div>
+
+				<div class="form-group">
+					{!! Form::label('password', 'Password:') !!}
+					{!! Form::password('password') !!}
+				</div>
+
+        	{!! Form::close() !!}
         </div>
     <!--</div>-->
 @stop
