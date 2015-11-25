5031113b3ad030c4553a0d11d00dc9141780e32f
vagrant <vagrant@homestead>
2015-11-25 16:46:15 +0000

    database

--- database/migrations/2015_11_25_160103_foreing_keys_indexs.php
+++ database/migrations/2015_11_25_160103_foreing_keys_indexs.php
@@ -12,6 +12,11 @@ class ForeingKeysIndexs extends Migration
      */
     public function up()
     {   
+      /*  Schema::table('militarUtilizador', function($table){
+            $table->foreign('militar_id')->references('id')->on('militars');
+            $table->foreign('utilizador_id')->references('id')->on('users');
+        }); */
+        
         Schema::table('militarUtilizador', function($table){
             $table->foreign('militar_id')->references('id')->on('militars');
             $table->foreign('utilizador_id')->references('id')->on('users');
